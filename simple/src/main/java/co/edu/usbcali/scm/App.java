6909b047aa512dfa8001df8925336160d36c5b93
rodrigo <rhurtadoc02@gmail.com>
2020-05-23 11:51:16 -0500

    se sube proyecto

---
+++ simple/src/main/java/co/edu/usbcali/scm/App.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.scm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ simple/src/test/java/co/edu/usbcali/scm/AppTest.java
@@ -0,0 +1,26 @@
+package co.edu.usbcali.scm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+	
+	@Test
+    public void shouldAnswerWithTrue_02()
+    {
+        assertTrue( true );
+    }
+}
